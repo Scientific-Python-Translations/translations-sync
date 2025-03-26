f4260296b830c8bcdecaa67afc730f70f264bc97
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-03-26 16:37:28 -0500

    Update upstream PR and create translations file

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+import yaml
 import os
 import traceback
 import tempfile
 from datetime import datetime
 from subprocess import Popen, PIPE
+from pathlib import Path
 
 from crowdin_api import CrowdinClient
 from github import Github, Auth
+
+
+example_dict = {
+    "es": [
+        {
+            "name": "Gonzalo Peña",
+            "username": "goanpeca",
+            "img_link": "goanpeca@gmail.com",
+        },
+        {
+            "name": "Gonzalo Peña",
+            "username": "goanpeca",
+            "img_link": "goanpeca@gmail.com",
+        },
+    ],
+    "pt_BR": [
+        {
+            "name": "Gonzalo Peña",
+            "username": "goanpeca",
+            "img_link": "goanpeca@gmail.com",
+        },
+    ],
+}
 
 
 def parse_input() -> dict:
@@ -73,6 +98,14 @@ class ScientificCrowdinClient:
             result[project["data"]["name"]] = project["data"]["id"]
         return result
 
+    def get_project_id(self, project_name):
+        projects = self._client.projects.with_fetch_all().list_projects()
+        for project in projects["data"]:
+            if project["data"]["name"] == project_name:
+                return project["data"]["id"]
+        else:
+            raise ValueError(f"Project '{project_name}' not found.")
+
     def get_project_status(self, project):
         results = {}
         for project_name, project_id in self.get_projects().items():
@@ -90,6 +123,14 @@ class ScientificCrowdinClient:
                     "approval": language["data"]["approvalProgress"],
                 }
         return results
+
+    def get_project_languages(self, project_name):
+        projects = self._client.projects.with_fetch_all().list_projects()
+        for project in projects["data"]:
+            if project["data"]["name"] == project_name:
+                return project["data"]["targetLanguageIds"]
+        else:
+            raise ValueError(f"Project '{project_name}' not found.")
 
     def get_valid_languages(
         self, project_name: str, translation_percentage: int, approval_percentage: int
@@ -126,6 +167,35 @@ class ScientificCrowdinClient:
                 }
         return valid_languages
 
+    def get_project_translators(self, project_name):
+        results = {}
+        project_id = self.get_project_id(project_name)
+        languages = self.get_project_languages(project_name)
+        for lang in sorted(languages):
+            results[lang] = []
+            offset = 0
+            limit = 500
+            while True:
+                items = self._client.string_translations.list_language_translations(
+                    lang, project_id, limit=limit, offset=offset
+                )
+                if data := items["data"]:
+                    for item in data:
+                        user_data = {
+                            "username": item["data"]["user"]["username"],
+                            "name": item["data"]["user"]["fullName"],
+                            "img_link": item["data"]["user"]["avatarUrl"].replace(
+                                "/medium/", "/large/"
+                            ),
+                        }
+                        if user_data not in results[lang]:
+                            results[lang].append(user_data)
+                    offset += limit
+                else:
+                    break
+
+        return results
+
 
 def configure_git_and_checkout_repos(
     username,
@@ -159,12 +229,18 @@ def configure_git_and_checkout_repos(
     email : str
         Email of the bot account.
     """
+    os.environ["GITHUB_TOKEN"] = token
     print("\n\n### Configure git information and checkout repositories")
     print("\n\ngetcwd:", os.getcwd())
+    run(["ls"])
 
     # Configure git information
     run(["git", "config", "--global", "user.name", f'"{name}"'])
     run(["git", "config", "--global", "user.email", f'"{email}"'])
+
+    _owner, repo = source_repo.split("/")
+    source_repo_fork = f"scientificpythontranslations/{repo}"
+    run(["gh", "repo", "fork", source_repo, "--clone=false"])
 
     if source_ref:
         cmds = [
@@ -173,7 +249,7 @@ def configure_git_and_checkout_repos(
             "--single-branch",
             "-b",
             source_ref,
-            f"https://{username}:{token}@github.com/{source_repo}.git",
+            f"https://{username}:{token}@github.com/{source_repo_fork}.git",
         ]
     else:
         cmds = [
@@ -183,6 +259,8 @@ def configure_git_and_checkout_repos(
         ]
 
     run(cmds)
+    print("\n\ngetcwd:", os.getcwd())
+    run(["ls"])
 
     if translations_ref:
         cmds = [
@@ -203,6 +281,58 @@ def configure_git_and_checkout_repos(
 
     os.chdir(translations_repo.split("/")[1])
     print("\n\ngetcwd:", os.getcwd())
+    run(["ls"])
+
+
+def verify_signature(token, repo, name, email, pr_title, branch_name) -> bool:
+    """Verify the signature of the pull request.
+
+    Parameters
+    ----------
+    token : str
+
+    repo : str
+        Repository name.
+    name : str
+        Name of the bot account.
+    email : str
+        Email of the bot account.
+    pr_title : str
+        Title of the pull request.
+    branch_name : str
+        Branch name of the pull request.
+    """
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+    repo = g.get_repo(repo)
+    pulls = repo.get_pulls(state="open", sort="created", direction="desc")
+    pr_branch = None
+    signed_by = f"{name} <{email}>"
+
+    for pr in pulls:
+        pr_branch = pr.head.ref
+        if pr.title == pr_title and pr_branch == branch_name:
+            print("\n\nFound PR try to merge it!")
+
+            # Check if commits are signed
+            checks = []
+            for commit in pr.get_commits():
+                print(
+                    [
+                        commit.commit.verification.verified,
+                        signed_by,
+                        commit.commit.verification.payload,
+                    ]
+                )
+                checks.append(
+                    commit.commit.verification.verified
+                    and signed_by in commit.commit.verification.payload
+                )
+            break
+
+    g.close()
+
+    return all(checks)
 
 
 def filter_commits(filename: str, language: str) -> None:
@@ -236,6 +366,7 @@ def create_pr_and_verify(self):
 
 
 def create_translations_pr(
+    username,
     token,
     source_repo,
     source_folder,
@@ -247,6 +378,7 @@ def create_translations_pr(
     email,
     language,
     language_code,
+    use_precommit,
 ):
     """Create a pull request for translations.
 
@@ -280,6 +412,7 @@ def create_translations_pr(
     out : str
         Output of the command.
     """
+    base_folder = Path(os.getcwd())
     print(f"\n\n### Creating PR for {language}")
     upstream_remote = "origin"
     source_branch = translations_ref
@@ -344,76 +477,155 @@ filter_commits('\\$filename', '{language}')
         run(["git", "push", "-u", "origin", branch_name])
         pr_title = f"Update translations for {language}"
         os.environ["GITHUB_TOKEN"] = token
-        run(
-            [
-                "gh",
-                "pr",
-                "create",
-                "--base",
-                "main",
-                "--head",
-                branch_name,
-                "--title",
-                pr_title,
-                "--body",
-                f"This PR to update translations for {language} was generated by the GitHub workflow, sync-translations.yml and includes all commits from this repo's Crowdin branch for the language of interest.",
-            ]
-        )
+        # run(
+        #     [
+        #         "gh",
+        #         "pr",
+        #         "create",
+        #         "--base",
+        #         "main",
+        #         "--head",
+        #         branch_name,
+        #         "--title",
+        #         pr_title,
+        #         "--body",
+        #         f"This PR to update translations for {language} was generated by the GitHub workflow, sync-translations.yml and includes all commits from this repo's Crowdin branch for the language of interest.",
+        #     ]
+        # )
 
-        auth = Auth.Token(token)
-        g = Github(auth=auth)
-        repo = g.get_repo(translations_repo)
-        pulls = repo.get_pulls(state="open", sort="created", direction="desc")
-        pr_branch = None
-        signed_by = f"{name} <{email}>"
+        if verify_signature(
+            token=token,
+            repo=translations_repo,
+            name=name,
+            email=email,
+            pr_title=pr_title,
+            branch_name=branch_name,
+        ):
+            print("\n\nAll commits are signed, auto-merging!")
+            # https://cli.github.com/manual/gh_pr_merge
+            os.environ["GITHUB_TOKEN"] = token
+            # run(["gh", "pr", "merge", branch_name, "--auto", "--squash", '--delete-branch'])
+        else:
+            print("\n\nNot all commits are signed, abort merge!")
 
-        for pr in pulls:
-            pr_branch = pr.head.ref
-            if pr.title == pr_title and pr_branch == branch_name:
-                print("\n\nFound PR try to merge it!")
+        # Create PR upstream
+        two_letter_lang_code = language_code[:2]
+        translations_branch_name = f"add/translations-{language_code}"
+        path = str(Path(os.getcwd()).parent / source_repo.split("/")[1])
+        trans_path = (
+            base_folder.parent / translations_folder
+        ).parent / two_letter_lang_code
+        dest_path = (
+            base_folder.parent / source_folder
+        ).parent  # / two_letter_lang_code
+        os.chdir(path)
+        run(["git", "checkout", "-b", translations_branch_name])
+        print("PATH:", trans_path)
+        run(["rsync", "-av", "--delete", str(trans_path), str(dest_path)])
 
-                # Check if commits are signed
-                checks = []
-                for commit in pr.get_commits():
-                    print(
-                        [
-                            commit.commit.verification.verified,
-                            signed_by,
-                            commit.commit.verification.payload,
-                        ]
-                    )
-                    checks.append(
-                        commit.commit.verification.verified
-                        and signed_by in commit.commit.verification.payload
-                    )
+        run(["git", "add", "."])
+        if use_precommit:
+            run(["pre-commit", "run", "--all-files"])
+            run(["git", "add", "."])
 
-                if all(checks):
-                    print("\n\nAll commits are signed, auto-merging!")
-                    # https://cli.github.com/manual/gh_pr_merge
-                    os.environ["GITHUB_TOKEN"] = token
-                    # run(["gh", "pr", "merge", branch_name, "--auto", "--squash"])
-                else:
-                    print("\n\nNot all commits are signed, abort merge!")
-
-                break
-
-        g.close()
+        _out, _err, rc = run(["git", "diff", "--staged", "--quiet"])
+        pr_title = f"Add translations for {language}"
+        if rc:
+            run(["git", "commit", "-S", "-m", f"Add {language} translations."])
+            run(["git", "push", "-u", "origin", translations_branch_name, "--force"])
+            os.environ["GITHUB_TOKEN"] = token
+            # gh pr create --repo owner/repo --base master --head user:patch-1
+            run(
+                [
+                    "gh",
+                    "pr",
+                    "create",
+                    "--repo",
+                    source_repo,
+                    "--base",
+                    "main",
+                    "--head",
+                    f"{username}:{translations_branch_name}",
+                    "--title",
+                    pr_title,
+                    "--body",
+                    f"This PR adds the translations for {language} to the project.\n\nThis PR was automatically created by the @scientificpythontranslations bot and no commits should be pushed directly to this branch/PR. Any modifications should be addressed directly in the https://scientific-python.crowdin.com/ site.\n\nThe Crowdin integration for this repository is located at https://github.com/{translations_repo}.",
+                ]
+            )
+        run(["git", "checkout", "main"])
+        os.chdir(base_folder)
     else:
         if rc != 0:
             print("\n\nNothing to cherry-pick.")
             print(out, err)
 
 
+def create_translators_file(translators, token, name, email, translations_repo):
+    """Create a file with the translators information.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    print("\n\n### Creating translators file")
+    with open("translators.yml", "w") as fh:
+        fh.write(yaml.dump(translators, default_flow_style=False, allow_unicode=True))
+
+    branch_name = "add/translators-file"
+    pr_title = "Add/update traslations file."
+    run(["git", "checkout", "-b", "add/translators-file"])
+    run(["git", "add", "."])
+    run(["git", "commit", "-S", "-m", pr_title])
+    run(["git", "push", "-u", "origin", branch_name, "--force"])
+    run(
+        [
+            "gh",
+            "pr",
+            "create",
+            "--base",
+            "main",
+            "--head",
+            branch_name,
+            "--title",
+            pr_title,
+            "--body",
+            "Update translations file.",
+        ]
+    )
+    if verify_signature(
+        token=token,
+        repo=translations_repo,
+        name=name,
+        email=email,
+        pr_title=pr_title,
+        branch_name=branch_name,
+    ):
+        print("\n\nAll commits are signed, auto-merging!")
+        # https://cli.github.com/manual/gh_pr_merge
+        os.environ["GITHUB_TOKEN"] = token
+        # run(["gh", "pr", "merge", branch_name, "--auto", "--squash", '--delete-branch'])
+    else:
+        print("\n\nNot all commits are signed, abort merge!")
+
+
 def main():
     try:
         gh_input = parse_input()
+        crowdin_project = gh_input["crowdin_project"]
         client = ScientificCrowdinClient(
             token=gh_input["crowdin_token"], organization="Scientific-python"
         )
-        valid_languages = client.get_valid_languages(
-            gh_input["crowdin_project"],
-            int(gh_input["translation_percentage"]),
-            int(gh_input["approval_percentage"]),
+        # valid_languages = client.get_valid_languages(
+        #     crowdin_project,
+        #     int(gh_input["translation_percentage"]),
+        #     int(gh_input["approval_percentage"]),
+        # )
+        translators = client.get_project_translators(
+            crowdin_project,
         )
         configure_git_and_checkout_repos(
             username=gh_input["username"],
@@ -425,20 +637,29 @@ def main():
             name=gh_input["name"],
             email=gh_input["email"],
         )
-        for language_code, data in valid_languages.items():
-            create_translations_pr(
-                token=gh_input["token"],
-                source_repo=gh_input["source_repo"],
-                source_folder=gh_input["source_folder"],
-                source_ref=gh_input["source_ref"],
-                translations_repo=gh_input["translations_repo"],
-                translations_folder=gh_input["translations_folder"],
-                translations_ref=gh_input["translations_ref"],
-                name=gh_input["name"],
-                email=gh_input["email"],
-                language=data["language_name"],
-                language_code=language_code,
-            )
+        # for language_code, data in valid_languages.items():
+        #     create_translations_pr(
+        #         username=gh_input["username"],
+        #         token=gh_input["token"],
+        #         source_repo=gh_input["source_repo"],
+        #         source_folder=gh_input["source_folder"],
+        #         source_ref=gh_input["source_ref"],
+        #         translations_repo=gh_input["translations_repo"],
+        #         translations_folder=gh_input["translations_folder"],
+        #         translations_ref=gh_input["translations_ref"],
+        #         name=gh_input["name"],
+        #         email=gh_input["email"],
+        #         language=data["language_name"],
+        #         language_code=language_code,
+        #         use_precommit=gh_input["use_precommit"],
+        #     )
+        create_translators_file(
+            translators,
+            token=gh_input["token"],
+            name=gh_input["name"],
+            email=gh_input["email"],
+            translations_repo=gh_input["translations_repo"],
+        )
     except Exception as e:
         print("Error: ", e)
         print(traceback.format_exc())
